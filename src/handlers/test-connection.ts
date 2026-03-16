f54f4bb72ed810f310f39121141b283cb3962dd4
chukwutem-emi <chukwutememi@gmail.com>
2026-03-16 20:43:57 +0100

    updated

--- src/handlers/test-connection.ts
+++ src/handlers/test-connection.ts
@@ -6,18 +6,21 @@ import path from "path";
 export const handler = async () => {
   try {
     console.log("File in /var/task:", fs.readdirSync("/var/task"));
+    console.log("Connecting to:", process.env.DB_HOST, process.env.DB_PORT);
     const connection = await mysql.createConnection({
       host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       ssl: { ca: fs.readFileSync(path.join(process.cwd(), "certificate/ca.pem")) }, // path relative to Lambda
     });
 
-    await connection.execute("SELECT 1");
+    const [rows] = await connection.execute("SELECT 1");
+    console.log("ROWS", rows);
     console.log("✅ Connected to Aiven MySQL!");
-    
-    await connection.end(); // close connection
+    await  connection.end();
+    return rows;
   } catch (err) {
     console.error("❌ Connection failed:", err);
   }
